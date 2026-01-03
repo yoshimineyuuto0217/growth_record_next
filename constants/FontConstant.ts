13fd38151788d788d6f65977b23924090243ba1b
吉嶺勇斗 <jilingyongdou@gmail.com>
2026-01-03 23:54:47 +0900

    add：定数ファイルを作成

---
+++ constants/FontConstant.ts
@@ -0,0 +1,6 @@
+// 各ページの見出しのサイズを決めてるもの
+export const HEADING_STYLES = {
+  small: "text-2xl font-bold font-(--font-noto-sans) mb-5",
+  middle: "text-4xl font-bold font-(--font-noto-sans)",
+  lage: "text-6xl font-bold font-(--font-noto-sans)"
+};
