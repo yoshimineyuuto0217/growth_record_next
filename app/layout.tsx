b09b0714f0efe77438d4d20181bdb0ee2ce63839
吉嶺勇斗 <jilingyongdou@gmail.com>
2026-01-05 00:04:42 +0900

    add：footerの追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +27,11 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-black min-h-screen flex flex-col`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-black min-h-screen flex flex-col `}
       >
         <Header />
         <main className="grow flex">{children}</main>
+        <Footer/>
       </body>
     </html>
   );
