ae912f68082c4f172c2138b594d618f17aee99b7
吉嶺勇斗 <jilingyongdou@gmail.com>
2026-01-04 16:33:54 +0900

    refactor:定数ファイルを修正

--- constants/InputBox.ts
+++ constants/InputBox.ts
@@ -1,6 +1,6 @@
 // 各ページの見出しのサイズを決めてるもの
 export const INPUTBOX_STYLES = {
   small: "",
-  middle: "w-175 h-12.5 border border-[#000000] rounded-[5px]",
+  middle: "w-175 h-12.5 border border-[#000000] rounded-[5px] pl-2 ",
   lage: ""
 };
