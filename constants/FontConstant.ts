5f3c48330f24e1e8106ab1f67847e23c15daa49e
吉嶺勇斗 <jilingyongdou@gmail.com>
2026-01-04 16:54:42 +0900

    refactor:定数ファイルの修正

--- constants/FontConstant.ts
+++ constants/FontConstant.ts
@@ -1,6 +1,6 @@
 // 各ページの見出しのサイズを決めてるもの
 export const HEADING_STYLES = {
-  small: "text-2xl font-bold font-(--font-noto-sans) mb-5",
+  small: "text-2xl font-bold font-(--font-noto-sans) ",
   middle: "text-4xl font-bold font-(--font-noto-sans)",
   lage: "text-6xl font-bold font-(--font-noto-sans)"
 };
