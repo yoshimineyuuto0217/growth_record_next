f561744eca4b2a8dc9e43949ff306f2ed9dc88e4
吉嶺勇斗 <jilingyongdou@gmail.com>
2026-01-07 01:43:03 +0900

    add：記事の投稿・年度別のページ作成

---
+++ app/articles/[id]/page.tsx
@@ -0,0 +1,8 @@
+// 記事詳細ページ
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
--- features/common/articles/IconBox.tsx
+++ features/common/articles/IconBox.tsx
@@ -4,16 +4,23 @@ const IconBox = ({
   src,
   alt,
   srcclassname,
+  asType = "a",
 }: {
   src: string;
   alt: string;
   srcclassname: string;
+  asType?: "a" | "button";
 }) => {
   return (
     <div className={srcclassname}>
-      <a href="/profile">
-        <Image src={src} alt={alt} width={100} height={100} />
-      </a>
+      {asType === "a" && (
+        <a href="/profile">
+          <Image src={src} alt={alt} width={100} height={100} />
+        </a>
+      )}
+      {asType === "button" && (
+          <Image src={src} alt={alt} width={100} height={100} />
+      )}
     </div>
   );
 };
