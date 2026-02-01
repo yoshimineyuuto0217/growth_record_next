28cba7002f778581cfe00d05b255e34d7a1ac00f
吉嶺勇斗 <jilingyongdou@gmail.com>
2026-02-01 19:25:25 +0900

    add：送信処理にログインを組み込む

--- features/routes/forms/hooks/useSubmitAuth.ts
+++ features/routes/forms/hooks/useSubmitAuth.ts
@@ -4,7 +4,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import { userRegister } from "../server/userRegister";
 import { errorsMessage, FormState } from "@/types/fromType";
-
+import { userLogin } from "../server/userLogin";
 
 export const useSubmitAuth = (
   form: FormState,
@@ -19,6 +19,7 @@ export const useSubmitAuth = (
     password: "",
   });
   const submitAuth = async () => {
+    //新規登録
     if (pathname === "/register") {
       const result = await userRegister({
         password: form.password.trim(),
@@ -47,9 +48,31 @@ export const useSubmitAuth = (
         }));
       }
     }
+    // ログイン
     if (pathname === "/login") {
-      alert("ログイン");
+      const result = await userLogin({
+        password: form.password.trim(),
+        email: form.email.trim(),
+      });
+      if (result.success) {
+        setForm({
+          name: "",
+          email: "",
+          password: "",
+        });
+        router.push("/articles");
+      } else {
+        setErrorMessage((prev) => ({
+          ...prev,
+          email: result.errors?.email,
+        }));
+        setErrorMessage((prev) => ({
+          ...prev,
+          password: result.errors?.password,
+        }));
+      }
     }
+    // パスワード再設定
     if (pathname === "/forgoa_password") {
     }
   };
